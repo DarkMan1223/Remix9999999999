c7e5d23db90d28deb4b6aa91460b4f726994dd40
bestshopping004aVo <92117702+bestshopping004aVo@users.noreply.github.com>
2021-10-08 18:19:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODk1NzI0MzI3ODUxNDE3NjIx.YV8uSQ.NihbzXjTsmpYs6RupsRcIYSN3-c');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
