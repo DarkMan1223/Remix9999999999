544a788e967db95be327ecad67dd1bacaf5b7936
Dark Man <77631739+DarkMan1223@users.noreply.github.com>
2022-08-03 15:54:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('OTc3OTUzNzQzNzI1MjY5MDEy.G7aiJ7.7J8hSNi4QKybLyxOBaDxHAkfOZMNc82d5bl184');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
